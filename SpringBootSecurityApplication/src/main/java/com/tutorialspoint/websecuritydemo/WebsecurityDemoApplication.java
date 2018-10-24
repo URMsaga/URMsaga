d2ddfa8666676fa8eec586b6a8e1e836c527d933
rauppath <rcreddy4java@gmail.com>
2018-10-24 17:08:13 +0530

    added springbootsecurityapplication

---
+++ SpringBootSecurityApplication/src/main/java/com/tutorialspoint/websecuritydemo/WebsecurityDemoApplication.java
@@ -0,0 +1,11 @@
+package com.tutorialspoint.websecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsecurityDemoApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(WebsecurityDemoApplication.class, args);
+   }
+}
